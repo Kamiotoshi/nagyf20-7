259ef71c7372a0e437146e5e21750c592b7fc82a
Kamiotoshi <133650059+Kamiotoshi@users.noreply.github.com>
2023-07-20 10:21:13 +0700

    Update web.js

--- ngay20-7/web.js
+++ ngay20-7/web.js
@@ -1,24 +1,14 @@
-function Guide(){
-    var Ten = document.getElementById("Ten").value;
-    var email = document.getElementById("email").value;
-    var Phone = document.getElementById("Phone").value;
-    var Gender =document.getElementById("Gender").value;
-    var Love = document.getElementById("Love").value;
-    var Review =document.getElementById("Review").value;
+document.getElementById("1").addEventListener("submit", function(thong_bao) {
+    thong_bao.preventDefault();
 
-    var bao="";
-    if (Ten && email && Phone && Gender && Love && Review ) {
-        
-        
-        
-        bao+= "Chúc mừng bạn " + Ten +  " đã đăng ký thành công và là tân sinh viên năm 2099 của FPT Aptech";
-        bao+= "Chúng tôi sẽ gửi đầy đủ thông tin cho bạn tại email " + email;
-        bao+= "Mọi thông tin bổ sung chúng tôi sẽ liên lạc cho bạn tại số điện thoại " + Phone; 
-            
-        
-       
-    }
+var name = document.getElementById("Ten").value;
+var email = document.getElementById("email").value;
+var phone = document.getElementById("Phone").value;
 
-
-    document.getElementById("Thongbao").innerHTML = bao;
-    return false;}
+if (name && email && phone) {
+    document.getElementById("Thongbao").innerHTML += "Chúc mừng bạn " + name +  " đã đăng ký thành công và là tân sinh viên năm 2099 của FPT Aptech";
+    document.getElementById("Thongbao").innerHTML += "Chúng tôi sẽ gửi đầy đủ thông tin cho bạn tại email " + email;
+    document.getElementById("Thongbao").innerHTML += "Mọi thông tin bổ sung chúng tôi sẽ liên lạc cho bạn tại số điện thoại " + phone; 
+    document.getElementById("1").reset();
+}
+});
